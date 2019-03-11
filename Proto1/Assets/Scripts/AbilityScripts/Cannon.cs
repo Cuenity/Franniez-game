2b385d47540c73cbd718ca0aad2dcf40b54ba5ac
Anne _ <anne1993@live.nl>
2019-03-11 13:01:00 +0100

    Nu wel met de abilities scene

---
+++ Proto1/Assets/Scripts/AbilityScripts/Cannon.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cannon : MonoBehaviour
+{
+    public bool containsBall = false;
+    public Cannon cannonTube;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+        {
+            
+        }
+    }
+}
