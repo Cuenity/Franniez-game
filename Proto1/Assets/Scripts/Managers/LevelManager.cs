8a9dce1204b564d6e2361402eeb0ea521acfcc8d
Jasper <jasper.van.zanten@spritecloud.com>
2019-03-28 14:17:21 +0100

    added dingen

---
+++ Proto1/Assets/Scripts/Managers/LevelManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    PlayerManager PlayerManager;
+    PlatformManager platformManager;
+    private void Awake()
+    {
+        PlayerManager = gameObject.AddComponent<PlayerManager>();
+        platformManager = gameObject.AddComponent<PlatformManager>();
+        platformManager.spawnLevel1();
+        platformManager.Init_Platforms();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+   
+}
