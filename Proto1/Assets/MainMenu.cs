597d2eb369316f3b5573d87ced02b332940d8985
Mitch Heijmann <mitchheijmann@ziggo.nl>
2019-03-29 15:55:28 +0100

    Menu toegevoegd
    Dat

---
+++ Proto1/Assets/MainMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public void StartGame()
+    {
+        Debug.Log("Start Game");
+
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Debug.Log("Exit game");
+            Application.Quit();
+        }
+    }
+}
