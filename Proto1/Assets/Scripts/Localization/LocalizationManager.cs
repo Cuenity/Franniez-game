ef0e6d4e78dd178573022b2e341db757b7b935d8
Mitch Heijmann <mitchheijmann@ziggo.nl>
2019-04-01 14:51:49 +0200

    Localization Done
    Klik op window en dan zie hem er tussen staan.

--- Proto1/Assets/Scripts/Localization/LocalizationManager.cs
+++ Proto1/Assets/Scripts/Localization/LocalizationManager.cs
@@ -5,12 +5,30 @@ using System.IO;
 
 public class LocalizationManager : MonoBehaviour
 {
+    public static LocalizationManager instance;
+
+    public enum Language { Dutch, English, Spanish };
     private Dictionary<string, string> localizedText;
+    //private Language Language;
+    private Language LanguageChoice;
+    private bool isReady = false;
+    private string missingTextString = "Localized text not found";
+
+
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (this != null)
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
     }
 
     public void LoadLocalizedText(string filename)
@@ -27,13 +45,71 @@ public class LocalizationManager : MonoBehaviour
             {
                 localizedText.Add(loadedData.items[i].key, loadedData.items[i].value);
             }
-            Debug.Log("Data is geladen.");
+
+            SetLanguage();
+            //Debug.Log("Data is geladen.");
         }
         else
         {
 
             Debug.LogError("Cannot find file!");
         }
+
+        isReady = true;
         
     }
+
+    private void SetLanguage()
+    {
+        string key = "language";
+        if(localizedText == null){ return;}
+        if (localizedText.ContainsKey(key))
+        {
+            string languageValue = localizedText[key].ToLower();
+
+            if (languageValue == "dutch")
+            {
+                LanguageChoice = Language.Dutch;
+                Debug.Log("Taal is Nederlands");
+            }
+            else if (languageValue == "english")
+            {
+                LanguageChoice = Language.English;
+                Debug.Log("Taal is Engels");
+            }
+            else if (languageValue == "spanish")
+            {
+                LanguageChoice = Language.Spanish;
+                Debug.Log("Taal is Spaans");
+            }
+
+        }
+
+   
+
+    }
+
+    public string GetLocalizedValue(string key)
+    {
+        string result = missingTextString;
+        if (localizedText.ContainsKey(key))
+        {
+            result = localizedText[key];
+        }
+
+        return result;
+
+    }
+
+    public bool GetisReady()
+    {
+        return isReady;
+    }
+
+    public Language GetLanguage()
+    {
+        return LanguageChoice;
+    }
+
+
 }
