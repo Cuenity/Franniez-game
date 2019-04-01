b22e5e1ce97f4ffd27f8e3bb8fea4ad8d027fd08
Jasper <jasper.van.zanten@spritecloud.com>
2019-04-01 15:11:25 +0200

    zoom in out camera works, started y,z dragging.

--- Proto1/Assets/Scripts/CameraClass.cs
+++ Proto1/Assets/Scripts/CameraClass.cs
@@ -14,6 +14,8 @@ public class CameraClass : MonoBehaviour
 
     public float SpringForce;
     public float SpringDamper;
+    public float dragSpeed = 2;
+    private Vector3 dragOrigin;
 
     private void Start()
     {
@@ -37,5 +39,39 @@ public class CameraClass : MonoBehaviour
 
             transform.LookAt(Target.transform.position + TargetLookAtOffset);
         }
+        if (gameState.BuildingPhaseActive == true)
+        {
+            Vector2 mousedata = Input.mouseScrollDelta;
+            if (mousedata.y > 0)
+            {
+                //zoomin
+                Vector3 cameraposition = gameState.cameraClass.transform.position;
+                if (cameraposition.x <= -5)
+                    gameState.cameraClass.transform.position = new Vector3(cameraposition.x + 1, cameraposition.y, cameraposition.z);
+            }
+            else if (mousedata.y < 0)
+            {
+                //zoomout
+                Vector3 cameraposition = gameState.cameraClass.transform.position;
+                if (cameraposition.x >= -20) //level groote
+                    gameState.cameraClass.transform.position = new Vector3(cameraposition.x - 1, cameraposition.y, cameraposition.z);
+            }
+        }
+
+        //if (Input.GetMouseButtonDown(0))
+        //{
+        //    dragOrigin = Input.mousePosition;
+        //    return;
+        //}
+
+        //if (!Input.GetMouseButton(0)) return;
+        //Vector3 pos = new Vector3(gameState.cameraClass.transform.position.x, Input.mousePosition.y - dragOrigin.y, Input.mousePosition.z - dragOrigin.z);
+        //Vector3 move = new Vector3(pos.x * dragSpeed, 0, pos.y * dragSpeed);
+
+        //transform.Translate(move, Space.World);
     }
+
 }
+
+
+
