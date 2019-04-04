4137a8f998ea6b91a4c40d11549f9f5465ef1a87
Mitch Heijmann <mitchheijmann@ziggo.nl>
2019-04-04 12:31:29 +0200

    Bug fix jeweet

--- Proto1/Assets/Scripts/Managers/StartupManager.cs
+++ Proto1/Assets/Scripts/Managers/StartupManager.cs
@@ -7,21 +7,28 @@ public class StartupManager : MonoBehaviour
 {
     public PlayerDataController dataController;
     // Start is called before the first frame update
-    public IEnumerator Start()
+    public void Start()
     {
-       while(!LocalizationManager.instance.GetisReady())
-        {
-            // Wacht 1 frame
-            yield return null;
-        }
+       //while(!LocalizationManager.instance.GetisReady())
+       // {
+       //     // Wacht 1 frame
+       //     yield return null;
+       // }
+    }
 
+    public void OnEnable()
+    {
+        LocalizationManager.ClickedButton += ButtonClicked;
+    }
+
+    public void ButtonClicked()
+    {
         Player player = new Player();
         player.name = "";
         player.language = (int)LocalizationManager.instance.LanguageChoice;
         PlayerDataController.instance.player = player;
 
         SceneManager.LoadScene("StartMenu");
-        
     }
 }
 
