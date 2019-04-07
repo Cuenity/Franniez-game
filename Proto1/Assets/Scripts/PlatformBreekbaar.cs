71ba7164489d810cbc3a34f0e00bfae3dbbb3d0c
Anne _ <anne1993@live.nl>
2019-04-07 12:06:14 +0200

    Breekbaar platform basis.

---
+++ Proto1/Assets/Scripts/PlatformBreekbaar.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformBreekbaar : MonoBehaviour
+{
+    Vector3 spawnPosition;
+    bool hasCollided;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameObject.transform.position!=spawnPosition && GameState.Instance.BuildingPhaseActive&&hasCollided)
+        {
+            gameObject.transform.position = spawnPosition;
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+
+    }
+    
+    private void OnCollisionExit(Collision collision)
+    {
+        gameObject.SetActive(false);
+        spawnPosition = gameObject.transform.position;
+        gameObject.transform.position = new Vector3(-100, -100, -100);
+    }
+}
