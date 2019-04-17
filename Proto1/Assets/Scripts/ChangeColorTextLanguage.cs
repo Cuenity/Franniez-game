209a946f6bdd23a57599c60a8da7554a3ae5def7
Mitch Heijmann <mitchheijmann@ziggo.nl>
2019-04-17 13:38:57 +0200

    StartMenu inxl instellingen

---
+++ Proto1/Assets/Scripts/ChangeColorTextLanguage.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts
+{
+    class ChangeColorTextLanguage : MonoBehaviour
+    {
+        public Text EnglishText;
+        public Text DutchText;
+        public Text SpanishText;
+
+        private Color32 SelectedColor = new Color32(47, 140, 242, 255);
+
+        public void Awake()
+        {
+
+            SetColor();
+        }
+
+        void OnEnable()
+        {
+            MainMenu.ChangeLanguage += ChangeTextColor;
+        }
+
+        private void SetColor()
+        {
+            
+            switch (PlayerDataController.instance.player.language)
+            {
+                case 0:
+                    DutchText.color = SelectedColor;
+                    break;
+                case 1:
+                    EnglishText.color = SelectedColor;
+                    break;
+                case 2:
+                    SpanishText.color = SelectedColor;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public void ChangeTextColor()
+        {
+            DutchText.color = new Color(255, 255, 255, 255);
+            EnglishText.color = new Color(255, 255, 255, 255);
+            SpanishText.color = new Color(255, 255, 255, 255);
+            SetColor();
+        }
+
+    }
+}
---
+++ Proto1/Assets/Scripts/LanguageSettingsImages.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LanguageSettingsImages : MonoBehaviour
+{
+    public Sprite SpanishFlag;
+    public Sprite UKFlag;
+    public Sprite DutchFlag;
+
+    private Button button;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        button = GetComponent<Button>();
+        ChangeFlag();
+    }
+
+    private void OnEnable()
+    {
+        MainMenu.ChangeLanguage += ChangeFlag;
+    }
+
+    public void ChangeFlag()
+    {
+        switch (PlayerDataController.instance.player.language)
+        {
+            case 0:
+                button.GetComponent<Image>().sprite = DutchFlag;
+                break;
+            case 1:
+                button.GetComponent<Image>().sprite = UKFlag;
+                break;
+            case 2:
+                button.GetComponent<Image>().sprite = SpanishFlag;
+                break;
+            default:
+                break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
