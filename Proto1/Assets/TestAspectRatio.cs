f3a2685bc37a9ac9cfbe65c63ec86ee50ec075c5
Mitch Heijmann <mitchheijmann@ziggo.nl>
2019-04-22 17:23:47 +0200

    Fix voor beeld

---
+++ Proto1/Assets/TestAspectRatio.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestAspectRatio : MonoBehaviour
+{
+    public Text Play;
+    public Text Store;
+    public Text Settings;
+
+    //public Camera Camera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //if (Camera.main.aspect >= 2)
+        //{
+        //    RectTransform rectTransform;
+        //    rectTransform = Play.GetComponent<RectTransform>();
+        //    rectTransform.localPosition = new Vector3(136, -3, 0);
+        //    rectTransform.localScale = new Vector3(0.8f,0.8f,0);
+
+        //    rectTransform = Store.GetComponent<RectTransform>();
+        //    rectTransform.localPosition = new Vector3(-115.7f, -31, 0);
+        //    rectTransform.localScale = new Vector3(0.9f, 0.9f, 0);
+
+        //    rectTransform = Settings.GetComponent<RectTransform>();
+        //    rectTransform.localPosition = new Vector3(-119.29f, 53, 0);
+        //    rectTransform.localScale = new Vector3(0.8f, 0.8f, 0);
+        //}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
