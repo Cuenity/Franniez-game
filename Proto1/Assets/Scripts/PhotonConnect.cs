d3a925d085bc866130768ab69f1f02fd32527be2
Anne _ <anne1993@live.nl>
2019-04-25 14:47:27 +0200

    multiplayer is super leuk en werkt intuitief

--- Proto1/Assets/Scripts/PhotonConnect.cs
+++ Proto1/Assets/Scripts/PhotonConnect.cs
@@ -70,7 +70,7 @@ public class PhotonConnect : MonoBehaviourPunCallbacks
 
     public void onClickStartGame()
     {
-        PhotonNetwork.LoadLevel(6);
+        PhotonNetwork.LoadLevel(17);
     }
 
     //private void OnFailedToConnectToMasterServer(NetworkConnectionError error)
