c2c55c4be71cd752a782f25573a52f62d58bf7bb
Jasper <jasper.van.zanten@spritecloud.com>
2019-05-09 12:58:11 +0200

    fixed de trampoline

--- Proto1/Assets/Scripts/platformen/Trampoline.cs
+++ Proto1/Assets/Scripts/platformen/Trampoline.cs
@@ -23,68 +23,11 @@ public class Trampoline : Platform
         if (!collision.isTrigger)
         {
             GameObject player = gamestate.playerBallManager.activePlayer;
-            Vector3 velocity = player.GetComponent<Rigidbody>().velocity;
-            float velocityY;
-            float velocityx;
-            if (velocity.y > 0)
-            {
-                velocityY = velocity.y;
-            }
-            else
-            {
-                velocityY = velocity.y * -1;
-            }
-            if ((velocityY < 4 && velocityY > 0) || velocityY == 0)
-            {
-                if (gamestate.playerBallManager.activePlayer.name.Contains("Light"))
-                {
-                    velocityY = 2;
-                }
-                else
-                {
-                    velocityY = 4;
-                }
-            }
-            else if (velocityY > -4 && velocityY < 0)
-            {
-                if (gamestate.playerBallManager.activePlayer.name.Contains("Light"))
-                {
-                    velocityY = -2;
-                }
-                else
-                {
-                    velocityY = -4;
-                }
-            }
+            Rigidbody body = player.GetComponent<Rigidbody>();
+            Vector3 velocity = body.velocity;
+            body.velocity= new Vector3( velocity.x, 0, 0);           
 
-            if (velocity.x < 1 && velocity.x > 0)
-            {
-                if (gamestate.playerBallManager.activePlayer.name.Contains("Light"))
-                {
-                    velocityx = 1;
-                }
-                else
-                {
-                    velocityx = 2;
-                }
-            }
-            else if ((velocity.x > -1 && velocity.x < 0) || velocity.x == 0)
-            {
-                if (gamestate.playerBallManager.activePlayer.name.Contains("Light"))
-                {
-                    velocityx = -1;
-                }
-                else
-                {
-                    velocityx = -2;
-                }
-            }
-            else
-            {
-                velocityx = velocity.x;
-            }
-
-            player.GetComponent<Rigidbody>().AddForce(velocityx, velocityY * 6f, 0, ForceMode.Impulse);
+            body.AddForce(velocity.x, 36, 0, ForceMode.Impulse);
         }
     }
     private void OnCollisionEnter(Collision collision)
@@ -92,68 +35,11 @@ public class Trampoline : Platform
         if (PhotonNetwork.InRoom)
         {
             GameObject player = gamestate.playerBallManager.activePlayer;
-            Vector3 velocity = player.GetComponent<Rigidbody>().velocity;
-            float velocityY;
-            float velocityx;
-            if (velocity.y > 0)
-            {
-                velocityY = velocity.y;
-            }
-            else
-            {
-                velocityY = velocity.y * -1;
-            }
-            if ((velocityY < 4 && velocityY > 0) || velocityY == 0)
-            {
-                if (gamestate.playerBallManager.activePlayer.name.Contains("Light"))
-                {
-                    velocityY = 2;
-                }
-                else
-                {
-                    velocityY = 4;
-                }
-            }
-            else if (velocityY > -4 && velocityY < 0)
-            {
-                if (gamestate.playerBallManager.activePlayer.name.Contains("Light"))
-                {
-                    velocityY = -2;
-                }
-                else
-                {
-                    velocityY = -4;
-                }
-            }
+            Rigidbody body = player.GetComponent<Rigidbody>();
+            Vector3 velocity = body.velocity;
+            body.velocity = new Vector3(velocity.x, 0, 0);
 
-            if (velocity.x < 1 && velocity.x > 0)
-            {
-                if (gamestate.playerBallManager.activePlayer.name.Contains("Light"))
-                {
-                    velocityx = 1;
-                }
-                else
-                {
-                    velocityx = 2;
-                }
-            }
-            else if ((velocity.x > -1 && velocity.x < 0) || velocity.x == 0)
-            {
-                if (gamestate.playerBallManager.activePlayer.name.Contains("Light"))
-                {
-                    velocityx = -1;
-                }
-                else
-                {
-                    velocityx = -2;
-                }
-            }
-            else
-            {
-                velocityx = velocity.x;
-            }
-            velocityY = 8;
-            player.GetComponent<Rigidbody>().AddForce(velocityx, velocityY * 6f, 0, ForceMode.Impulse);
+            body.AddForce(velocity.x, 36, 0, ForceMode.Impulse);
         }
     }
 }
